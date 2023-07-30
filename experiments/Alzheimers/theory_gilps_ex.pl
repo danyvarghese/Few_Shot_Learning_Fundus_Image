549548912a80c8d87649d26abddfb3dd75ccfb1e
Dany Varghese <gagiprha@gmail.com>
2023-07-30 13:31:13 +0100

    Add files via upload

---
+++ experiments/Alzheimers/theory_gilps_ex.pl
@@ -0,0 +1,7 @@
+theory_clause([target(A),avr(A,B),torta(A,C),gteq(C,10558.451392)],_472913).
+theory_clause([target(A),avr(A,B),tortv(A,C),gteq(C,21116.902784)],_472955).
+theory_clause([target(A),tortv(A,B),gteq(B,9238.644968)],_472992).
+theory_clause([target(A),torta(A,B),gteq(B,26396.12848)],_473029).
+theory_clause([target(A),torta(A,B),gteq(B,44873.418416)],_473066).
+theory_clause([target(A),avr(A,B),torta(A,C),gteq(C,11878.257816)],_473108).
+theory_clause([target(A),bstda(A,B),tortv(A,C),gteq(C,25076.322056)],_473150).

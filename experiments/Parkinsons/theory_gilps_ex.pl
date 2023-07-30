a3b2cbe8002a422fad5485d68b1b31ac28fdabbd
Dany Varghese <gagiprha@gmail.com>
2023-07-30 13:46:43 +0100

    Delete theory_gilps_ex.pl

--- experiments/Parkinsons/theory_gilps_ex.pl
+++
@@ -1,7 +0,0 @@
-theory_clause([target(A),tortv(A,B),gteq(B,11878.257816)],_3835219).
-theory_clause([target(A),torta(A,B),gteq(B,22436.709208)],_3835256).
-theory_clause([target(A),bstda(A,B),torta(A,C),gteq(C,15837.677088)],_3835298).
-theory_clause([target(A),torta(A,B),gteq(B,18477.289936)],_3835335).
-theory_clause([target(A),avr(A,B),tortv(A,C),gteq(C,5279.225696)],_3835377).
-theory_clause([target(A),avr(A,B),torta(A,C),gteq(C,9238.644968)],_3835419).
-theory_clause([target(A),avr(A,B),tortv(A,C),gteq(C,15837.677088)],_3835461).
